084718753daa32ff4c21f91d0c00266df89eeed7
nanton83 <nicholas.romans@gmail.com>
2020-01-30 14:31:02 -0500

    frontend file structure built

--- FavRestaurants-api/db/seeds.rb
+++ FavRestaurants-api/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create([
+    {name: 'Over The Falls', city: 'Wake Forest', state: 'North Carolina'},
+    {name: 'Simons Restauarant', city: 'Horseheads', state: 'New York'},
+    {name: 'Stumble Inn', city: 'New York', state: 'New York'},
+    {name: 'Pita Pocket', city: 'Van Nuys', state: 'California'}
+])

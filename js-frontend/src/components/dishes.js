2788513fa6992fda75adf459ac43c5763ed8ba59
nanton83 <nicholas.romans@gmail.com>
2020-03-04 12:37:12 -0500

    able to find selected restaurant id

--- js-frontend/src/components/dishes.js
+++ js-frontend/src/components/dishes.js
@@ -10,8 +10,9 @@ class Dishes{
         this.dishForm = document.getElementById('new-dish-form')
         this.newDishName = document.getElementById('new-dish-name')
         this.restaurantId = document.getElementById('new-dish-restaurant-id')
-        this.selectElement = document.querySelector('restaurant-list')
+        
         this.restaurantBtn = document.getElementById("dishBtn").addEventListener("click", this.getDishOption)
+        
     }
 
     
@@ -19,7 +20,7 @@ class Dishes{
     createDish(event){
         event.preventDefault()
         const nameValue = this.newDishName.value
-        debugger
+        
         this.adapter.createDish(nameValue, restaurantId).then(dish => {
             this.dishes.push(new Dish(dish))
             
@@ -28,11 +29,13 @@ class Dishes{
     }
 
     getDishOption() { 
-// trying to get info from selected dropdown currently displaying undefined
 
-        let output = this.selectElement
+        this.optionIndex = document.getElementById("restaurant-list")
+        let restaurantId = this.optionIndex.options.selectedIndex
+        debugger
+    
         
-        console.log(output) 
+        console.log(restaurantId) 
     } 
 
 }
